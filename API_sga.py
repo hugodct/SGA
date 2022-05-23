928e3a7b674b99a819e1590223c29a7c524fbcab
hugodct <105286513+hugodct@users.noreply.github.com>
2022-05-23 13:57:21 +0200

    Add files via upload

--- API_sga.py
+++ API_sga.py
@@ -1,6 +1,9 @@
 from flask import Flask,request
 import algoritmos_sga as al
 import networkx as nx
+import pandas as pd
+pd.set_option('display.max_columns', None)
+
 
 app = Flask(__name__)
 
@@ -28,6 +31,53 @@ def do_picking():
     path_dict = {i: path[i] for i in range(0, len(path))}
 
     return path_dict
+
+
+@app.route('/gen_warehouse', methods=['POST'])
+def generate_warehouse():
+    data = request.get_json()
+    nodes_df = pd.DataFrame(columns=['Id', 'Codigo', 'Habilitado', 'IdAlmacen', 'PosX', 'PosY', 'PosZ', 'IdContenedor', 'color'])
+
+    for i in range(len(data)): #creacion del dataframe de nodos
+        ubi = data[i]
+        nodes_df.loc[i] = [ubi["Id"], ubi["Codigo"], ubi["Habilitado"], ubi["IdAlmacenInteligente"], ubi["PlanoCoordenadaX"], ubi["PlanoCoordenadaY"], ubi["PlanoCoordenadaZ"], ubi["IdContenedor"], 'blue']
+    print(nodes_df)
+
+    for i in range(0,len(data),2): #compresion para evitar coordenada Z
+        nodes_df.iloc[i,7] = nodes_df.iloc[i,7] + "," + nodes_df.iloc[i+1,7]
+    for i in range(0,len(data),2):
+        nodes_df = nodes_df.drop([i + 1])
+    nodes_df.drop(columns=['PosZ'], inplace=True)
+    print(nodes_df)
+
+
+    edges_df = pd.DataFrame(columns=['Source', 'Target', 'Type', 'weight', 'color'])
+    shape = [int(nodes_df['PosX'].max()),int(nodes_df['PosY'].max())]
+
+    k = int(0)  # Generar edges de columnas
+    for i in range(1, shape[0] + 1):
+        for j in range(1, shape[1]):
+            nodo_actual = str(i) + str(j + 1)
+            nodo_anterior = str(i) + str(j)
+            edges_df.loc[k] = [nodo_anterior, nodo_actual, "Undirected", 1, "black"]
+            k += 1
+
+    IPASILLOS = range(shape[1]+1)
+    for j in range(1, shape[1] + 1):  # Generar edges de filas
+        if j in IPASILLOS:
+            for i in range(1, shape[0]):
+                nodo_actual = str(i + 1) + str(j)
+                nodo_anterior = str(i) + str(j)
+                edges_df.loc[k] = [nodo_anterior, nodo_actual, "Undirected", 1, "black"]
+                k += 1
+
+    print(edges_df)
+
+
+    return {"yes":"test"}
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
