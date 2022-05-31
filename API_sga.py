46a5dda508a65fec81a08394c10cc8148b7a6120
hugodct <105286513+hugodct@users.noreply.github.com>
2022-05-31 09:38:20 +0200

    Add files via upload

--- API_sga.py
+++ API_sga.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-import algoritmos_sga as al
+import pickingLogic as al
 import networkx as nx
 import pandas as pd
 pd.set_option('display.max_columns', None)
@@ -7,76 +7,60 @@ pd.set_option('display.max_columns', None)
 
 app = Flask(__name__)
 
-@app.route('/picking',methods=['POST'])
-def do_picking():
-    dict_refs = request.args
-    res = dict_refs['SKU'].split(",")
-    refs = list(map(int, res))
+@app.route('/gen_warehouse', methods=['POST'])
+def generate_warehouse():
+    data = request.get_json()
 
-    NROWS = 7
-    NCOLUMNS = 10
-    IPASILLOS = [1, 4, 7]  # Filas en las que hay pasillo horizontal
+    nodes_df = al.generate_nodes(data)
+    edges_df = al.generate_edges(nodes_df)
+    print(nodes_df)
+    print(edges_df)
 
-    edges_df = al.generate_warehouse_edges(NROWS, NCOLUMNS, IPASILLOS)
-    nodes_df = al.generate_warehouse_nodes(NROWS, NCOLUMNS)
+    nodes_df.to_pickle(r'C:\Users\LENOVO\PycharmProjects\almacen\Nodes.pkl')
+    edges_df.to_pickle(r'C:\Users\LENOVO\PycharmProjects\almacen\Edges.pkl')
+
+    return "Warehouse generated successfully"
+
+
+@app.route('/container_picking', methods=['POST'])
+def do_container_picking():
+    edges_df = pd.read_pickle(r'C:\Users\LENOVO\PycharmProjects\almacen\Edges.pkl')
+    nodes_df = pd.read_pickle(r'C:\Users\LENOVO\PycharmProjects\almacen\Nodes.pkl')
+    container_list = request.get_json()
+    print(edges_df)
+    print(nodes_df)
 
     G = nx.from_pandas_edgelist(edges_df,
                                 source='Source',
                                 target='Target',
                                 edge_attr=["weight", "color"])
-    nx.set_node_attributes(G, 'blue', name='color')
 
-    path, nodos = al.get_references_tsp(edges_df, nodes_df, refs)
+    nodos = al.get_nodes_where_container(nodes_df, container_list, 0)
 
-    path_dict = {i: path[i] for i in range(0, len(path))}
+    tsp = nx.approximation.traveling_salesman_problem
+    path = tsp(G, nodes=nodos, cycle=True)
+
+    keys = list(range(1,len(path)))
+    path_dict = dict(zip(keys, path))
 
     return path_dict
 
 
-@app.route('/gen_warehouse', methods=['POST'])
-def generate_warehouse():
-    data = request.get_json()
-    nodes_df = pd.DataFrame(columns=['Id', 'Codigo', 'Habilitado', 'IdAlmacen', 'PosX', 'PosY', 'PosZ', 'IdContenedor', 'color'])
-
-    for i in range(len(data)): #creacion del dataframe de nodos
-        ubi = data[i]
-        nodes_df.loc[i] = [ubi["Id"], ubi["Codigo"], ubi["Habilitado"], ubi["IdAlmacenInteligente"], ubi["PlanoCoordenadaX"], ubi["PlanoCoordenadaY"], ubi["PlanoCoordenadaZ"], ubi["IdContenedor"], 'blue']
-    print(nodes_df)
-
-    for i in range(0,len(data),2): #compresion para evitar coordenada Z
-        nodes_df.iloc[i,7] = nodes_df.iloc[i,7] + "," + nodes_df.iloc[i+1,7]
-    for i in range(0,len(data),2):
-        nodes_df = nodes_df.drop([i + 1])
-    nodes_df.drop(columns=['PosZ'], inplace=True)
-    print(nodes_df)
-
-
-    edges_df = pd.DataFrame(columns=['Source', 'Target', 'Type', 'weight', 'color'])
-    shape = [int(nodes_df['PosX'].max()),int(nodes_df['PosY'].max())]
-
-    k = int(0)  # Generar edges de columnas
-    for i in range(1, shape[0] + 1):
-        for j in range(1, shape[1]):
-            nodo_actual = str(i) + str(j + 1)
-            nodo_anterior = str(i) + str(j)
-            edges_df.loc[k] = [nodo_anterior, nodo_actual, "Undirected", 1, "black"]
-            k += 1
-
-    IPASILLOS = range(shape[1]+1)
-    for j in range(1, shape[1] + 1):  # Generar edges de filas
-        if j in IPASILLOS:
-            for i in range(1, shape[0]):
-                nodo_actual = str(i + 1) + str(j)
-                nodo_anterior = str(i) + str(j)
-                edges_df.loc[k] = [nodo_anterior, nodo_actual, "Undirected", 1, "black"]
-                k += 1
+@app.route('/article_picking', methods=['POST'])
+def do_article_picking():
+    edges_df = pd.read_pickle(r'C:\Users\LENOVO\PycharmProjects\almacen\Edges.pkl')
+    nodes_df = pd.read_pickle(r'C:\Users\LENOVO\PycharmProjects\almacen\Nodes.pkl')
+    article_dict = request.get_json()
 
     print(edges_df)
+    print(nodes_df)
+    print(article_dict)
+
+    possible_retrievals = al.get_nodes_where_article(nodes_df, article_dict)
+    al.meta_heuristic(possible_retrievals)
 
 
-    return {"yes":"test"}
-
-
+    return article_dict
 
 
 if __name__ == "__main__":
--- pickingLogic.py
+++ pickingLogic.py
@@ -2,27 +2,33 @@ import pandas as pd
 import networkx as nx
 
 def generate_nodes(data):
-    nodes_df = pd.DataFrame(columns=['Id', 'Codigo', 'Habilitado', 'IdAlmacen', 'PosX', 'PosY', 'PosZ', 'IdContenedor'])
+    nodes_df = pd.DataFrame(columns=['Id', 'Codigo', 'Habilitado', 'IdAlmacen', 'PosX', 'PosY', 'PosZ',
+                                     'IdContenedor', 'ContenidoCodigo1','ContenidoCodigo2','ContenidoCodigo3','ContenidoUds1','ContenidoUds2','ContenidoUds3'])
 
-    for i in range(len(data)):  # creacion del dataframe de nodos
+    for i in range(len(data)):  #creacion del dataframe de nodos
         ubi = data[i]
         nodes_df.loc[i] = [ubi["Id"], ubi["Codigo"], ubi["Habilitado"], ubi["IdAlmacenInteligente"],
                            ubi["PlanoCoordenadaX"], ubi["PlanoCoordenadaY"], ubi["PlanoCoordenadaZ"],
-                           ubi["IdContenedor"]]
+                           ubi["IdContenedor"], ubi["ContenidoCodigo1"], ubi["ContenidoCodigo2"], ubi["ContenidoCodigo3"],
+                           ubi["ContenidoUds1"], ubi["ContenidoUds2"], ubi["ContenidoUds3"]]
+
+    nodes_df['ContenidoA'] = nodes_df["ContenidoCodigo1"].astype(str) + "," + nodes_df["ContenidoCodigo2"].astype(str) + "," + nodes_df["ContenidoCodigo3"].astype(str)
+    nodes_df['UnidadesA'] = nodes_df["ContenidoUds1"].astype(str) + "," + nodes_df["ContenidoUds2"].astype(str) + "," + nodes_df["ContenidoUds3"].astype(str)
 
     #MANUAL TABLE PIVOT
-    df2 = nodes_df.iloc[1::2]
-    contenedoresB = df2[['IdContenedor']]
-    contenedoresB.reset_index(drop=True, inplace=True)
-    contenedoresB.rename(columns={"IdContenedor": "IdContenedorB"}, inplace=True)
+    df2 = nodes_df.iloc[1::2] #select odd rows
+    height1_df = pd.concat([df2['IdContenedor'], df2['ContenidoA'], df2['UnidadesA']], axis=1, keys=['IdContenedorB', 'ContenidoB', 'UnidadesB'])
 
-    r = list(range(1, len(data), 2))
+    height1_df.reset_index(drop=True, inplace=True)
+
+    r = list(range(1, len(data), 2)) #[1,3,...,(2n-1)]
 
     nodes_df.drop(r, inplace=True)
     nodes_df.reset_index(drop=True, inplace=True)
 
-    nodes_df = nodes_df.join(contenedoresB)
-    nodes_df.drop(columns=['PosZ'], inplace=True)
+    nodes_df = nodes_df.join(height1_df)
+    nodes_df.drop(columns=['PosZ','ContenidoCodigo1','ContenidoCodigo2','ContenidoCodigo3','ContenidoUds1','ContenidoUds2','ContenidoUds3'], inplace=True)
+    nodes_df.rename(columns={'IdContenedor': 'IdContenedorA'}, inplace=True)
 
     nodes_df['NodeName'] = nodes_df["PosX"].astype(str) + nodes_df["PosY"].astype(str) #Generate nodename for tsp
 
@@ -54,3 +60,67 @@ def generate_edges(nodes_df):
                 k += 1
 
     return edges_df
+
+def get_nodes_where_container(nodes_df, containers, first_node):
+    indices = [first_node]  # Get nodes from list of containers
+
+    for i in range(len(containers)):
+        if str(containers[i]) in nodes_df['IdContenedorA'].values:
+            indice = nodes_df.index[nodes_df['IdContenedorA'] == str(containers[i])].tolist()
+        elif str(containers[i]) in nodes_df['IdContenedorB'].values:
+            indice = nodes_df.index[nodes_df['IdContenedorB'] == str(containers[i])].tolist()
+
+        indices.append(indice[0])
+
+    nodos = []  # Idem
+    for i in range(len(containers)+1):
+        nodo = nodes_df.iloc[indices[i], -1]
+        nodos.append(nodo)
+
+    return nodos
+
+
+def get_nodes_where_article(nodes_df, articles):
+    node_dict = {}  #Gets list of articles, returns node and height of possible retrieval
+
+    for i in range(len(articles)):
+        node_list = []
+
+        art = list(articles.keys())[i]
+        cantidad_solicitada = articles[art]
+
+        for j in range(nodes_df.shape[0]):
+            cont_A_string = nodes_df.loc[nodes_df.index[j],'ContenidoA'].split(',')
+            cont_B_string = nodes_df.loc[nodes_df.index[j],'ContenidoB'].split(',')
+
+            uds_A_string = nodes_df.loc[nodes_df.index[j],'UnidadesA'].split(',')
+            uds_B_string = nodes_df.loc[nodes_df.index[j], 'UnidadesB'].split(',')
+
+            A_dict = dict(zip(cont_A_string, uds_A_string))
+            B_dict = dict(zip(cont_B_string, uds_B_string))
+
+            if art in cont_A_string and A_dict[art] >= cantidad_solicitada:
+                node_list.append([nodes_df.loc[nodes_df.index[j],'NodeName'],'A'])
+
+            elif art in cont_B_string and B_dict[art] >= cantidad_solicitada:
+                node_list.append([nodes_df.loc[nodes_df.index[j],'NodeName'],'B'])
+
+        node_dict[art] = node_list
+
+    return node_dict
+
+def meta_heuristic(pr):
+    print(pr)
+    n = 1
+    for articulo in pr:
+        positions = len(pr[articulo])
+        n = n * positions
+
+    if n > 20:
+        return 0 #Too many combinations
+
+    else:
+        for i in range(n):
+            pass
+
+    pass
