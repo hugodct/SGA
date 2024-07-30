f146c752f9de5c146bcd65927d2040ee201a13c6
hugodct <hugodct@gmail.com>
2024-07-30 15:27:49 +0200

    improved graphics aesthetic

--- main_sga.py
+++ main_sga.py
@@ -17,9 +17,10 @@ G = nx.from_pandas_edgelist(edges_df,
                             source='Source',
                             target='Target',
                             edge_attr=["weight", "color"])
-nx.set_node_attributes(G, 'blue', name='color')
+nx.set_node_attributes(G, 'white', name='color')
+nx.set_edge_attributes(G, 'white', name='color')
 
-path, nodos = al.get_references_tsp(edges_df, nodes_df, [101,103,119,150,170])
+path, nodos = al.get_references_tsp(edges_df, nodes_df, [101,103,119,155,160])
 print('Nodos', nodos, 'Coste', len(path), 'Path', path)
 
 for i in range(len(path)-1): #Cambiar color del path
@@ -37,7 +38,8 @@ edge_widths = [x * 3 for x in edge_widths]
 knodes = G.nodes() #Get attributes of nodes
 knode_colors = [knodes[u]['color'] for u in knodes]
 
+plt.rcParams['axes.facecolor'] = '#202020'
 plt.figure()
-nx.draw_networkx(G,pos=pos, node_size = 350, node_color = knode_colors, linewidths=5, edge_color=edge_colors, width=edge_widths, font_size=10, font_family='Lucida Sans')
+nx.draw_networkx(G, pos=pos, node_size = 350, node_color = knode_colors, linewidths=5, edge_color=edge_colors, width=edge_widths, font_size=10, font_family='Lucida Sans')
 plt.show()
 
